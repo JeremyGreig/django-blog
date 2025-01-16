45ff8323abdd4fc3a04a50ff6af53eb0e20dc180
Jeremy Greig <jeremy.greig10@gmail.com>
2025-01-16 17:32:29 +0000

    Maybe fixed the 400 error

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5u@kje7um5%w^qfpajts4shrf!&9@n)6xs7%*enuwk@#b$vnt!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".herokuapp.com"
+    '.localhost',
+    '.herokuapp.com'
     ]
 
 
