01b1ba99e629ea0b5e0736c0aaf23744aa3df78b
Jeremy Greig <jeremy.greig10@gmail.com>
2025-01-20 19:34:04 +0000

    Copilot changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.urls import path
 from .models import Post
 
 # Register your models here.
 
 admin.site.register(Post)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # other paths...
+]
